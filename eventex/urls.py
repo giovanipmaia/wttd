6c3a58f4df71b9f31807c34709e5b1ee10434c08
Giovani <giovanipmaia@gmail.com>
2015-04-07 17:45:20 -0300

    Formulario

--- eventex/urls.py
+++ eventex/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
+
     url(r'^admin/', include(admin.site.urls)),
-    )
+)
 #]
