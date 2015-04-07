0b293b5527574f896281634a7a9060fa0a0c2149
Giovani <giovanipmaia@gmail.com>
2015-04-07 15:00:03 -0300

    Debug false

--- eventex/settings.py
+++ eventex/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
+from decouple import config
+from dj_database_url import parse as db_url
 from unipath import Path
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,12 +24,16 @@ BASE_DIR = Path(__file__).parent
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm+@#dfz^es9urqwrnfn9yaetirlgq5l07@elcgq)g)e6u@&fwr'
+#SECRET_KEY = 'm+@#dfz^es9urqwrnfn9yaetirlgq5l07@elcgq)g)e6u@&fwr'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -84,8 +89,12 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     #}
-    'default': dj_database_url.config(
-        default='sqlite:///' + BASE_DIR.child('db.sqlite3'))
+    #'default': dj_database_url.config(
+    #    default='sqlite:///' + BASE_DIR.child('db.sqlite3'))
+    'default': config(
+        'DATABASE_URL',
+        default = 'sqlite:///' + BASE_DIR.child('db.sqlite3'),
+        cast=db_url),
 }
 
 
