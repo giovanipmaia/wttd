176c71fd97a3d8c0502771408b9c80020cf528f0
Giovani <giovanipmaia@gmail.com>
2015-04-09 17:27:37 -0300

    fim

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from eventex.core.managers import KindContactManager
 
 
 # Create your models here.
@@ -24,5 +25,19 @@ class Contact(models.Model):
     kind = models.CharField(_('tipo'), max_length=1, choices=KINDS)
     value = models.CharField(_('valor'), max_length=255)
 
+    objects = models.Manager()
+    emails = KindContactManager('E')
+    phones = KindContactManager('P')
+    faxes = KindContactManager('F')
+
     def __unicode__(self):
         return self.value
+
+
+class Talk(models.Model):
+    title = models.CharField(_(u'Título'), max_length=200)
+    description = models.TextField(_(u'Descrição'))
+    start_time = models.TimeField(_(u'Horário'), blank=True)
+
+    def __unicode__(self):
+        return self.title
